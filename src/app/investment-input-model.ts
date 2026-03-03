057e7e0ce0103a9cd1283ddd222e116b272b5723
simplisoni <simplisoni@users.noreply.github.com>
2026-03-03 15:00:36 +0530

    investment-calculator

---
+++ src/app/investment-input-model.ts
@@ -0,0 +1,14 @@
+export interface InvestmentInput {
+    year: number;
+    yearlyInvestment: number;
+    totalInvestment: number;
+    interest: number;
+    totalValue: number;
+}
+
+// type InvestmentInputData = {
+//     initialInvestment: number;
+//     annualInvestment: number;
+//     expectedReturn: number;
+//     duration: number;
+// }
